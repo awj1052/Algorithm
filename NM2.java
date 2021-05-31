bb932cc4b2ff7ea29d392ada812c06cb6baa9a77
awj1052 <awj1052@naver.com>
2021-06-01 04:51:54 +0900

    BAEKJOON day9

---
+++ NM2.java
@@ -0,0 +1,44 @@
+package day4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class NM2 {
+	// https://www.acmicpc.net/problem/15650
+	// 중복 x 수열
+	static boolean[] check;
+	static int N;
+	static int M;
+	static int[] visit;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		check = new boolean[N]; // 배열의 크기가 N+1 이고
+		visit = new int[N]; //
+		m(0, 0);
+		bw.close();
+	}
+	static void m(int n, int m) throws Exception {
+		if(n==M) { //
+			for(int i=0; i<M; i++) { // 1부터 시작한다면
+				bw.write((visit[i]+1) + " "); // +1을 안해도 됨
+			}
+			bw.newLine();
+			return;
+		}
+		for(int i=m; i<N; i++) { //
+			if(!check[i]) {
+				check[i]=true;
+				visit[n]=i;
+				m(n+1, i+1);
+				check[i]=false;
+			}
+		}
+	}
+}
