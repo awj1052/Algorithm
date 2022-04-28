a231a399e5b6b5e268d5036df22435b2ee08486d
안우진 <82976440+awj1052@users.noreply.github.com>
2022-04-28 17:44:49 +0900

    BOJ 13173

---
+++ BOJ13173.py
@@ -0,0 +1,28 @@
+# 점화식 -> 일반항
+
+def mpow(n,y):
+    p=n%C; res=1
+    while y:
+        if y%2: res=res*p%C
+        p=p*p%C
+        y//=2
+    return res
+
+C=10**9+7
+P=int(input())
+Q=int(input())
+N=int(input())
+K=int(input())
+if K==N: print(1)
+elif K==0: print(0)
+elif Q==P: print(0)
+elif Q==0: print(1)
+else:
+    if P==2*Q:
+        print(K*mpow(N,C-2)%C)
+    else:
+        a=Q*mpow(P,C-2)%C
+        b=a*mpow(1-a+C,C-2)%C
+        num = 1 - mpow(b,K) + C
+        den = 1 - mpow(b,N) + C
+        print(num*mpow(den,C-2)%C)
